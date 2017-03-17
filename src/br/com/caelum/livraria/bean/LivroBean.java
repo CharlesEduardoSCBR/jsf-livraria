89f3975bb20d27b6b0c38b43c79916075d64ad96
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-03-16 21:06:39 -0300

    C3M2.17.01 - Separação das responsabilidades do modelo e da view

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -5,44 +5,13 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 public class LivroBean {
 
-	private String titulo;
-	private String isbn;
-	private double preco;
-	private String dataLancamento;
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getIsbn() {
-		return isbn;
-	}
-
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-
-	public double getPreco() {
-		return preco;
-	}
-
-	public void setPreco(double preco) {
-		this.preco = preco;
-	}
-
-	public String getDataLancamento() {
-		return dataLancamento;
-	}
-
-	public void setDataLancamento(String dataLancamento) {
-		this.dataLancamento = dataLancamento;
+	private Livro livro = new Livro();
+	
+	public Livro getLivro(){
+		return livro;
 	}
 
 	public void gravar() {
-		System.out.println("Gravando livro " + this.titulo);
+		System.out.println("Gravando livro " + this.livro.getTitulo());
 	}
 }
