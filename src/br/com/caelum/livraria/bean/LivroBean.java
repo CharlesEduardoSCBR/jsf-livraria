88b48eedb8c18b482c147a654dc6266380d6c3b0
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-03-20 20:59:12 -0300

    C3M4.12.01 - FacesMessages Vs RuntimeException

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -3,8 +3,12 @@ package br.com.caelum.livraria.bean;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.validation.ValidationException;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -42,17 +46,19 @@ public class LivroBean implements Serializable {
 		return new DAO<Livro>(Livro.class).listaTodos();
 	}
 
-	public void gravarAutor() {
+	public void gravarAutor(){
 		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(autorId);
 		this.livro.adicionaAutor(autor);
 		System.out.println("Libro escrito por.:" + autor.getNome());
 	}
 
-	public void gravar() {
+	public void gravar(){
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor."));
+			return;
 		}
 	}
+	
 }
