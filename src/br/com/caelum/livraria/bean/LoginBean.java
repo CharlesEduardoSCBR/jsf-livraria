dc0298138a40023926fe426cfd28d3cdc5270c54
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-03-24 19:40:19 -0300

    C3M12.05.01 - Maos a obra: Implementando a autorização

--- src/br/com/caelum/livraria/bean/LoginBean.java
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,6 +2,7 @@ package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -19,9 +20,11 @@ public class LoginBean {
 	public String efetuaLogin(){
 		System.out.println("Fazendo login do usuario " + this.usuario.getEmail());
 		
+		FacesContext context = FacesContext.getCurrentInstance();
 		boolean existe = new UsuarioDao().existe(this.usuario);
 		
 		if(existe){
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return "livro?faces-redirect=true";
 		}
 		
