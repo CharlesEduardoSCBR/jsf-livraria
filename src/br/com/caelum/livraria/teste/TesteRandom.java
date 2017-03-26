4b68445ca76899b25d9d1c5c74c7ca7861aa986d
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-03-26 15:08:04 -0300

    C3M8.05 - Para saber mais: Um pouco de java.util.Random

---
+++ src/br/com/caelum/livraria/teste/TesteRandom.java
@@ -0,0 +1,25 @@
+package br.com.caelum.livraria.teste;
+
+import java.util.Random;
+
+public class TesteRandom {
+
+	public static void main(String[] args) {
+		
+		Random gerador = new Random();
+		
+		int resultado = gerador.nextInt(10);
+		System.out.println(resultado);
+		
+		int resultado2 = gerador.nextInt(10);
+		System.out.println(resultado2);
+		
+		Random geradorBoolean = new Random(10);
+		
+		boolean valor = geradorBoolean.nextBoolean();
+		System.out.println(valor);
+		
+		boolean valor2 = geradorBoolean.nextBoolean();
+		System.out.println(valor2);
+	}
+}
